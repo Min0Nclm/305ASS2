17593d5e5744f76335f38a7f52c77839b618d3c9
Min0Nclm <minodxck@gmail.com>
2025-11-27 14:32:21 +0800

    debug

--- resnet.py
+++ resnet.py
@@ -108,7 +108,7 @@ class ResNet(nn.Module):
             layers.append(block(self.inplanes, planes, groups=self.groups, base_width=self.base_width, dilation=self.dilation, norm_layer=norm_layer))
         return nn.Sequential(*layers)
     def forward(self, x):
-        x = self.conv1(x); x = self.bn1(x); x = self.relu(x); x = self.maxpool(x)
+        x = self.conv1(x); x = self.bn1(x); x = self.relu(x)
         x = self.layer1(x); x = self.layer2(x); x = self.layer3(x); x = self.layer4(x)
         x = self.avgpool(x); x = torch.flatten(x, 1); x = self.fc(x)
         return x
